1ee8def16240c835bfc0c86cb8dcd8f523469769
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2021-11-03 15:25:12 -0300

    Refactors scheduler to use object method as ticker.

--- pkg/jobs/scheduler.go
+++ pkg/jobs/scheduler.go
@@ -8,34 +8,30 @@ import (
 )
 
 // Scheduler manages the ticket object and fires/stops async jobs in background.
-type Scheduler interface {
-	Start()
-	Stop()
+type Scheduler struct {
+	ticker *time.Ticker
+	done   chan bool
+	f func() error
 }
 
-// NewScheduler creates a new instance of concrete implementation of scheduler.
-func NewScheduler() Scheduler {
-	return &scheduler{
+// NewScheduler creates a new instance of scheduler with an specific f function
+// to tick.
+func NewScheduler() *Scheduler {
+	return &Scheduler{
 		ticker: time.NewTicker(configs.GetJobSeconds()),
 		done:   make(chan bool),
+		f: tick,
 	}
 }
 
-type scheduler struct {
-	ticker *time.Ticker
-	done   chan bool
-}
-
-func (s *scheduler) Start() {
+func (s *Scheduler) Start() {
 	go func() {
 		for {
 			select {
 			case <-s.done:
 				return
 			case <-s.ticker.C:
-				log.Printf("")
-				if err := answer(); err != nil {
-					log.Fatalf("answer method failed: %v", err)
+				if err := s.f(); err != nil {
 					panic(err)
 				}
 			}
@@ -43,7 +39,15 @@ func (s *scheduler) Start() {
 	}()
 }
 
-func (s *scheduler) Stop() {
+func (s *Scheduler) Stop() {
 	s.ticker.Stop()
 	s.done <- true
+}
+
+func tick() error {
+	if err := answer(); err != nil {
+		log.Fatalf("answer method failed: %v", err)
+		return err
+	}
+	return nil
 }
