44716067d75042d3e4d3d0c09a73a7dacfdcebc1
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2021-11-03 13:13:07 -0300

    Adds security group to enable SSH connections.

--- deployments/main.tf
+++ deployments/main.tf
@@ -14,22 +14,52 @@ provider "aws" {
   region  = "us-east-2"
 }
 
+resource "aws_security_group" "main" {
+  egress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
+  ingress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 22
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 22
+    }
+  ]
+}
+
 resource "aws_instance" "bot_server" {
-  ami           = "ami-0b59bfac6be064b78" # See available AMIs here: https://aws.amazon.com/amazon-linux-ami/
-  instance_type = "t2.nano"
-  count         = 1
-  key_name      = "bot-server"
+  ami                    = "ami-0b59bfac6be064b78" # See available AMIs here: https://aws.amazon.com/amazon-linux-ami/
+  instance_type          = "t2.nano"
+  count                  = 1
+  key_name               = "bot-server"
+  vpc_security_group_ids = [aws_security_group.main.id]
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("~/.ssh/bot-server.pem")
+    host        = self.public_ip
+  }
 
   provisioner "file" {
     source      = "../.env"
-    destination = "/root/.env"
-
-    connection {
-      type        = "ssh"
-      user        = "root"
-      private_key = file("~/.ssh/bot-server.pem")
-      host        = self.public_ip
-    }
+    destination = "/home/ec2-user/.env"
   }
 
   provisioner "remote-exec" {
@@ -37,15 +67,8 @@ resource "aws_instance" "bot_server" {
       "sudo yum update -y",
       "sudo yum install docker -y",
       "sudo service docker start",
-      "sudo docker run --env-file /root/.env -d ariel17/twitter-echo-bot:latest"
+      "sudo docker run --env-file=/home/ec2-user/.env -d ariel17/twitter-echo-bot:latest"
     ]
-
-    connection {
-      type        = "ssh"
-      user        = "root"
-      private_key = file("~/.ssh/bot-server.pem")
-      host        = self.public_ip
-    }
   }
 
   tags = {
