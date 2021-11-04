f8bcd38934a551a07bbdedb9a081fa5d59550cf5
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2021-11-04 09:32:32 -0300

    Adds dynamic text as response with tests.

--- pkg/jobs/answers_test.go
+++ pkg/jobs/answers_test.go
@@ -17,7 +17,7 @@ func TestAnswer(t *testing.T) {
 		searchErr    error
 		answerErr    error
 	}{
-		{"ok", true, []twitter.Tweet{{ID: 1, Text: "hello!"}}, nil, nil},
+		{"ok", true, []twitter.Tweet{{ID: 1, Text: "hello!", UserName: "ariel17"}}, nil, nil},
 		{"failed by search error", false, nil, errors.New("mocked search error"), nil},
 		{"failed by answer error", false, []twitter.Tweet{{ID: 1, Text: "hello!"}}, nil, errors.New("mocked answer error")},
 	}
@@ -37,6 +37,31 @@ func TestAnswer(t *testing.T) {
 			if tc.answerErr != nil {
 				assert.Equal(t, tc.answerErr, err)
 			}
+		})
+	}
+}
+
+func TestCreateText(t *testing.T) {
+	tweet := twitter.Tweet{
+		UserName: "ariel17",
+	}
+	testCases := []struct {
+		name     string
+		greet    string
+		response string
+		tweet    twitter.Tweet
+		expected string
+	}{
+		{"response with greeting", "hola", "hellou", tweet, "hola @ariel17 hellou"},
+		{"response without greeting", "", "hellou", tweet, "@ariel17 hellou"},
+		{"response without greeting", "", "hellou", tweet, "@ariel17 hellou"},
+		{"greeting without response", "hola", "", tweet, "hola @ariel17"},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			text := createText(tc.tweet, tc.greet, tc.response)
+			assert.Equal(t, tc.expected, text)
 		})
 	}
 }
