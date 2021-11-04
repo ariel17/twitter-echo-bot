61eb87d26e52f673728a31c97e3677eb7bc1801a
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2021-11-04 09:32:07 -0300

    Adds greet env key and tests.

--- pkg/configs/jobs_test.go
+++ pkg/configs/jobs_test.go
@@ -13,28 +13,33 @@ func TestLoadJobKeys(t *testing.T) {
 	testCases := []struct{
 		name string
 		query string
+		greet string
 		response string
 		seconds string
 		isSuccessful bool
 	}{
-		{"ok", "query", "response", "99", true},
-		{"query cannot be empty", "", "response", "99", false},
-		{"response cannot be empty", "query", "", "99", false},
-		{"seconds cannot be empty", "query", "response", "", false},
-		{"seconds cannot be negative", "query", "response", "-10", false},
+		{"ok", "query", "greet", "response", "99", true},
+		{"query cannot be empty", "", "greet", "response", "99", false},
+		{"greet can be empty", "query", "", "response", "99", true},
+		{"response cannot be empty", "query", "greet", "", "99", false},
+		{"seconds cannot be empty", "query", "greet", "response", "", false},
+		{"seconds cannot be negative", "query", "greet", "response", "-10", false},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			oldQuery := os.Getenv(searchQueryKey)
+			oldGreet := os.Getenv(greetTextKey)
 			oldResponse := os.Getenv(responseTextKey)
 			oldSeconds := os.Getenv(jobSecondsKey)
 			defer func() {
 				os.Setenv(searchQueryKey, oldQuery)
+				os.Setenv(greetTextKey, oldGreet)
 				os.Setenv(responseTextKey, oldResponse)
 				os.Setenv(jobSecondsKey, oldSeconds)
 			}()
 			os.Setenv(searchQueryKey, tc.query)
+			os.Setenv(greetTextKey, tc.greet)
 			os.Setenv(responseTextKey, tc.response)
 			os.Setenv(jobSecondsKey, tc.seconds)
 
@@ -43,6 +48,7 @@ func TestLoadJobKeys(t *testing.T) {
 
 			if tc.isSuccessful {
 				assert.Equal(t, tc.query, GetSearchQuery())
+				assert.Equal(t, tc.greet, GetGreetText())
 				assert.Equal(t, tc.response, GetResponseText())
 				v, _ := strconv.Atoi(tc.seconds)
 				assert.Equal(t, time.Duration(v) * time.Second, GetJobSeconds())
